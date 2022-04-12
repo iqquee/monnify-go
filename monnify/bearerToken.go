30d13b8d13b61616c5d545cae33f46e5e9d5ac10
hisyntax <hisyntax0@gmail.com>
2022-04-12 11:11:43 +0100

    reserve acct function implemented

--- monnify/bearerToken.go
+++ monnify/bearerToken.go
@@ -1,6 +1,7 @@
 package monnify
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,7 +10,32 @@ import (
 	"github.com/hisyntax/go-monnify/helper"
 )
 
-func BearerToken(base_url, apiKey, SecretKey string) (string, error) {
+var Base_url string = "https://sandbox.monnify.com"
+
+func GetBearerToken() {
+	api_key := "MK_TEST_GVMPXQZ1FK"
+	secret_key := "99QPF9WCMGC488SYS2YWVHWEZ9R2JD7E"
+	BearerGenToken, err := BearerToken(Base_url, api_key, secret_key)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(BearerGenToken)
+}
+
+type Token struct {
+	RequestSuccessful bool
+	ResponseMessage   string
+	ResponseCode      string
+	ResponseBody      TokenBody
+}
+
+type TokenBody struct {
+	AccessToken string
+	ExpiresIn   int
+}
+
+func BearerToken(base_url, apiKey, SecretKey string) (*Token, error) {
 	api_secret_key := fmt.Sprintf("%s:%s", apiKey, SecretKey)
 	basic_auth := helper.Base64(api_secret_key)
 
@@ -23,7 +49,7 @@ func BearerToken(base_url, apiKey, SecretKey string) (string, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(helper.ServerErr)
-		return "", err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -32,5 +58,7 @@ func BearerToken(base_url, apiKey, SecretKey string) (string, error) {
 	log.Println(resp.Status)
 	log.Println(string(resp_body))
 
-	return string(resp_body), nil
+	var token Token
+	json.Unmarshal(resp_body, &token)
+	return &token, nil
 }
--- monnify/reserveAcct.go
+++ monnify/reserveAcct.go
@@ -37,37 +37,21 @@ type ReserveAcctResponseBody struct {
 	RestrictPaymentSource bool
 }
 
-type ReserveAccoutReq struct {
-	AccountName       string              `json:"account_name"`
-	AccountReference  string              `json:"account_reference"`
-	CurrencyCode      string              `json:"currency_code"`
-	ContractCode      string              `json:"contract_code"`
-	CustomerName      string              `json:"customer_name"`
-	CustomerEmail     string              `json:"customer_email"`
-	IncomeSplitConfig []IncomeSplitConfig `json:"income_split_config"`
-}
-
-type IncomeSplitConfig struct {
-	SubAccountCode  string `json:"Sub_account_code"`
-	SplitPercentage int    `json:"split_percentage"`
-	FeePercentage   int    `json:"fee_percentage"`
-	FeeBearer       bool   `jsos:"fee_bearer"`
-}
-
 var Client = &http.Client{}
 
-func ReserveAcct(base_url, bearer_auth string) (*ReserveAcctResponse, string, error) {
+func ReserveAcct(base_url, BearerToken, acctName, acctRef, contractcode, customerName, customerEmail string) (*ReserveAcctResponse, string, error) {
+
+	bearer_token := fmt.Sprintf("Bearer %s", BearerToken)
 	client := Client
 	url := fmt.Sprintf("%s/api/v1/bank-transfer/reserved-accounts", base_url)
-	var reserveAccoutReq ReserveAccoutReq
-	body, err := json.Marshal(&reserveAccoutReq)
+	body := []byte(fmt.Sprintf("{\n  \"accountName\": \"%s\",\n  \"accountReference\": \"%s\",\n  \"currencyCode\": \"NGN\",\n  \"contractCode\": \"%s\",\n  \"customerName\": \"%s\",\n  \"customerEmail\": \"%s\"\n}", acctName, acctRef, contractcode, customerName, customerEmail))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, "", err
 	}
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
-
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", bearer_auth))
+	req.Header.Add("Authorization", bearer_token)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -76,13 +60,18 @@ func ReserveAcct(base_url, bearer_auth string) (*ReserveAcctResponse, string, er
 	}
 
 	defer resp.Body.Close()
-	resp_body, _ := ioutil.ReadAll(resp.Body)
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
 
 	log.Println(resp.Status)
 	log.Println(string(resp_body))
 
 	var reservedAcct ReserveAcctResponse
 	json.Unmarshal(resp_body, &reservedAcct)
+
+	log.Println(reservedAcct.ResponseBody.AccountName)
 
 	return &reservedAcct, resp.Status, nil
 }
