7262d8f2cd7981d219a1a2d79a94b80828942cf9
hisyntax <hisyntax0@gmail.com>
2022-04-14 13:52:05 +0100

    model created to unmarshal the incoming response into for the bank transfer

--- monnify/bankTransfer.go
+++ monnify/bankTransfer.go
@@ -2,17 +2,44 @@ package monnify
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/hisyntax/go-monnify/helper"
 )
 
-func PayWithBankTransfer(bearerToken, base_url, acctRef string) {
+type PayWithBank struct {
+	RequestSuccessful bool
+	ResponseMessage   string
+	ResponseCode      string
+	ResponseBody      PayWithBankBody
+}
+
+type PayWithBankBody struct {
+	AccountNumber          string
+	AccountName            string
+	BankName               string
+	BankCode               string
+	AccountDurationSeconds int
+	UssdPayment            string
+	RequestTime            string
+	TransactionReference   string
+	PaymentReference       string
+	Amount                 int
+	Fee                    int
+	TotalPayable           int
+	CollectionChannel      string
+	ProductInformation     string
+}
+
+func PayWithBankTransfer(bearerToken, base_url, acctRef, bankCode string) (*PayWithBank, string, error) {
 	bearer_token := fmt.Sprintf("Bearer %s", bearerToken)
 	client := Client
 
 	url := fmt.Sprintf("%s/api/v1/merchant/bank-transfer/init-payment", base_url)
-	body := []byte(fmt.Sprintf("{\n  \"transactionReference\": \"%s\",\n  \"bankCode\": \"058\"\n}", acctRef))
+	body := []byte(fmt.Sprintf("{\n  \"transactionReference\": \"%s\",\n  \"bankCode\": \"%s\"\n}", acctRef, bankCode))
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
 
@@ -22,8 +49,8 @@ func PayWithBankTransfer(bearerToken, base_url, acctRef string) {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		fmt.Println("Errored when sending request to the server")
-		return
+		fmt.Println(helper.ServerErr)
+		return nil, "", err
 	}
 
 	defer resp.Body.Close()
@@ -31,4 +58,8 @@ func PayWithBankTransfer(bearerToken, base_url, acctRef string) {
 
 	fmt.Println(resp.Status)
 	fmt.Println(string(resp_body))
+
+	var payWithBank PayWithBank
+	json.Unmarshal(resp_body, &payWithBank)
+	return &payWithBank, resp.Status, nil
 }
