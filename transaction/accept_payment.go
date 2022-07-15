fc8877adb9d1a980dcd2a9621d7268e93d9ff616
hisyntax <hisyntax0@gmail.com>
2022-07-15 17:02:01 +0100

    implemented accept payment

--- transaction/accept_payment.go
+++ transaction/accept_payment.go
@@ -1,5 +1,15 @@
 package transaction
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/hisyntax/go-monnify"
+)
+
 type AcceptPaymentModel struct {
 	PaymentReference    string `json:"paymentReference"`
 	Amount              int    `json:"amount"`
@@ -31,6 +41,72 @@ type InitTransacStatusBody struct {
 	PaymentMethod        string
 }
 
-func AcceptPayment(amount int, paymentReference, paymentDesc, currencyCode, contractCode, customerName, customerEmail, customerPhoneNumber, redirectUrl string) {
-	// var
+type AcceptPaymentRes struct {
+	RequestSuccessful bool
+	ResponseMessage   string
+	ResponseCode      string
+	ResponseBody      AcceptPaymentResBody
+}
+
+type AcceptPaymentResBody struct {
+	TransactionReference string
+	PaymentReference     string
+	MerchantName         string
+	ApiKey               string
+	EnabledPaymentMethod []string
+	CheckoutUrl          string
+}
+
+func AcceptPayment(amount int, paymentReference, paymentDesc, currencyCode, contractCode, customerName, customerEmail, customerPhoneNumber, redirectUrl string) (*AcceptPaymentRes, int, error) {
+	client := monnify.NewClient()
+	url := fmt.Sprintf("%s/api/v1/merchant/transactions/init-transaction", client.Options.BaseUrl)
+	method := "POST"
+	payload := AcceptPaymentModel{
+		PaymentReference:    paymentReference,
+		Amount:              amount,
+		CurrencyCode:        currencyCode,
+		ContractCode:        contractCode,
+		CustomerEmail:       customerEmail,
+		CustomerName:        customerName,
+		CustomerPhoneNumber: customerPhoneNumber,
+		RedirectUrl:         redirectUrl,
+		PaymentDescription:  paymentDesc,
+	}
+	// payload := AcceptPaymentModel{}
+	// payload.PaymentReference = paymentReference
+	// payload.Amount = amount
+	// payload.CurrencyCode = currencyCode
+	// payload.ContractCode = contractCode
+	// payload.CustomerEmail = customerEmail
+	// payload.CustomerName = customerName
+	// payload.CustomerPhoneNumber = customerPhoneNumber
+	// payload.RedirectUrl = redirectUrl
+	// payload.PaymentDescription = paymentDesc
+
+	jsonReq, jsonReqErr := json.Marshal(&payload)
+	if jsonReqErr != nil {
+		return nil, 0, jsonReqErr
+	}
+
+	req, reqErr := http.NewRequest(method, url, bytes.NewBuffer(jsonReq))
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", client.BasicToken)
+
+	resp, respErr := client.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var response AcceptPaymentRes
+	if err := json.Unmarshal(resp_body, &response); err != nil {
+		return nil, 0, err
+	}
+
+	return &response, resp.StatusCode, nil
 }
