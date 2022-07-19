24b05136e57029a764e781596f4851f98155daf5
hisyntax <hisyntax0@gmail.com>
2022-07-19 14:44:06 +0100

    go mod updated to use this current repo name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/hisyntax/go-monnify
+module github.com/hisyntax/monnify-go
 
-go 1.17
+go 1.18
--- transaction/accept_payment.go
+++ transaction/accept_payment.go
@@ -7,7 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/hisyntax/go-monnify"
+	"github.com/hisyntax/monnify-go"
 )
 
 type AcceptPaymentModel struct {
